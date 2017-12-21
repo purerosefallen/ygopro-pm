33d6413d5ecd160564572424cc5f7be711c366c8
Hello23-Ygopro <loleh32@hotmail.com>
2017-12-21 02:29:12 +0200

    new card Double Colorless Energy

---
+++ script/c26001096.lua
@@ -0,0 +1,7 @@
+--Double Colorless Energy (Base Set 96/102)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnableEnergyAttribute(c)
+end
+scard.pokemon_card=true
