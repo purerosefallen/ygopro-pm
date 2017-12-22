a3910e52b49eb67c7d669c4272f722aa466c2a24
Hello23-Ygopro <loleh32@hotmail.com>
2017-12-23 00:59:22 +0200

    new cards special
    - Blaine's Quiz #1 (Gym Heroes 97/132)
    - Cedric Juniper (Legendary Treasures 110/113)
    - Puzzle of Time (BREAKpoint 109/122)
    https://github.com/Hello23-Ygopro/ygopro-pm/issues/4

---
+++ script/c26006097.lua
@@ -0,0 +1,37 @@
+--Blaine's Quiz #1 (Gym Heroes 97/132)
+--Not fully implemented: Cannot announce decimals with Duel.AnnounceNumber
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+os=require('os')
+function scard.initial_effect(c)
+	--draw
+	pm.EnableTrainerActivate(c,0,scard.drtg,scard.drop)
+end
+scard.pokemon_card=true
+function scard.filter(c)
+	return c.length and (c:IsBasicPokemon() or c:IsEvolution()) and c:IsSecret()
+end
+function scard.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(scard.filter,tp,LOCATION_HAND,0,1,nil) end
+end
+function scard.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(sid,0))
+	local g=Duel.SelectMatchingCard(tp,scard.filter,tp,LOCATION_HAND,0,1,1,nil)
+	if g:GetCount()==0 then return end
+	local tc=g:GetFirst()
+	Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEDOWN,true)
+	Duel.Hint(HINT_CODE,0,tc:GetCode())
+	local end_time=0
+	local start_time=os.time()
+	Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(sid,1))
+	local an=Duel.AnnounceNumber(1-tp,table.unpack(pm.length_list))
+	end_time=os.time()-start_time
+	if end_time>10 or an~=math.floor(tc.length) then
+		Duel.Draw(tp,2,REASON_EFFECT)
+	else
+		Duel.ChangePosition(tc,POS_FACEUP)
+		Duel.HintSelection(g)
+		Duel.Draw(1-tp,2,REASON_EFFECT)
+	end
+	Duel.SendtoHand(tc,PLAYER_OWNER,REASON_RULE)
+end
---
+++ script/c26067109.lua
@@ -0,0 +1,39 @@
+--Puzzle of Time (BREAKpoint 109/122)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	--sort deck or to hand
+	pm.EnableTrainerActivate(c,0,scard.opttg,scard.optop)
+end
+scard.pokemon_card=true
+function scard.opttg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingTarget(nil,tp,LOCATION_DECK,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(Card.IsAbleToHand,tp,PM_LOCATION_DPILE,0,1,nil)
+		and Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_HAND,0,1,e:GetHandler(),CARD_PUZZLE_OF_TIME)
+	if chk==0 then return b1 or b2 end
+	local opt=0
+	if b1 and b2 then
+		opt=Duel.SelectOption(tp,aux.Stringid(sid,0),aux.Stringid(sid,1))
+	elseif b1 then
+		opt=Duel.SelectOption(tp,aux.Stringid(sid,0))
+	else
+		opt=Duel.SelectOption(tp,aux.Stringid(sid,1))+1
+	end
+	if opt==1 then e:SetCategory(PM_CATEGORY_RECYCLE) end
+	e:SetLabel(opt)
+end
+function scard.optop(e,tp,eg,ep,ev,re,r,rp)
+	local opt=e:GetLabel()
+	if opt==0 then
+		Duel.SortDecktop(tp,tp,3)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_TOHAND)
+		local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,PM_LOCATION_DPILE,0,2,2,nil)
+		if g:GetCount()>0 then
+			Duel.SendtoHand(g,PLAYER_OWNER,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+		local tc=Duel.GetFirstMatchingCard(Card.IsCode,tp,LOCATION_HAND,0,nil,CARD_PUZZLE_OF_TIME)
+		Duel.SendtoDPile(tc,REASON_EFFECT+REASON_DISCARD)
+	end
+end
