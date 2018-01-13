4c839b557e9221c104d3dca328f3e6b85e1c1b37
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-13 02:34:39 +0200

    errata Pokémon Fan Club
    Search your deck for up to 2 Basic Pokémon and put them onto your Bench. Shuffle your deck afterward.
    → Search your deck for up to 2 Basic Pokémon, reveal them, and put them into your hand. Shuffle your deck afterward.

--- expansions/script/c2608009.lua
+++ expansions/script/c2608009.lua
@@ -2,26 +2,22 @@
 local pm=require "expansions.utility_ptcg"
 local scard,sid=pm.GetID()
 function scard.initial_effect(c)
-	--to bench
-	pm.EnableTrainerPlay(c,nil,scard.tbtg,scard.tbop)
+	--search
+	pm.EnableTrainerPlay(c,nil,scard.thtg,scard.thop)
 end
 scard.pokemon_card=true
-scard.old_supporter=true
-function scard.tbfilter(c,e,tp)
-	return c:IsBasicPokemon() and c:IsCanBePutInPlay(e,0,tp,false,false)
+function scard.thfilter(c)
+	return c:IsBasicPokemon() and c:IsAbleToHand()
 end
-function scard.tbtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,PM_LOCATION_BENCH)>0
-		and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+function scard.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
 end
-function scard.tbop(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,PM_LOCATION_BENCH)
-	if ft<=0 then return end
-	if ft>2 then ft=2 end
-	local g=Duel.GetMatchingGroup(scard.tbfilter,tp,LOCATION_DECK,0,nil,e,tp)
+function scard.thop(e,tp,eg,ep,ev,re,r,rp)
 	pm.ConfirmDeck(tp,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_TOHAND)
+	local g=Duel.SelectMatchingCard(tp,scard.thfilter,tp,LOCATION_DECK,0,1,2,nil)
 	if g:GetCount()==0 then return Duel.Hint(HINT_MESSAGE,tp,PM_DESC_NO_TARGETS) end
-	Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_TOBENCH)
-	local sg=g:Select(tp,1,ft,nil)
-	Duel.PutInPlay(sg,0,tp,tp,false,false,PM_POS_FACEUP_UPSIDE)
+	Duel.SendtoHand(g,PLAYER_OWNER,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
 end
+
