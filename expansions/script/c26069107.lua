3be76dad6b0a0d6b600d6eacbd72cb8df48409da
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-13 02:57:07 +0200

    alias Pokémon Fan Club

---
+++ expansions/script/c26069107.lua
@@ -0,0 +1,22 @@
+--Pokémon Fan Club (Fates Collide 107/124)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	--search
+	pm.EnableTrainerPlay(c,nil,scard.thtg,scard.thop)
+end
+scard.pokemon_card=true
+function scard.thfilter(c)
+	return c:IsBasicPokemon() and c:IsAbleToHand()
+end
+function scard.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+end
+function scard.thop(e,tp,eg,ep,ev,re,r,rp)
+	pm.ConfirmDeck(tp,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_TOHAND)
+	local g=Duel.SelectMatchingCard(tp,scard.thfilter,tp,LOCATION_DECK,0,1,2,nil)
+	if g:GetCount()==0 then return Duel.Hint(HINT_MESSAGE,tp,PM_DESC_NO_TARGETS) end
+	Duel.SendtoHand(g,PLAYER_OWNER,REASON_EFFECT)
+	Duel.ConfirmCards(1-tp,g)
+end
