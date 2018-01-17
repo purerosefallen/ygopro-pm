fbe70091a56da73ab4b0eed9416c5420c7e4abb9
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-18 00:09:26 +0200

    add missing Basic Energy

---
+++ expansions/script/c26001097.lua
@@ -0,0 +1,7 @@
+--Fighting Energy (Base Set 97/102)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnableEnergyAttribute(c)
+end
+scard.pokemon_card=true
