9b00bd7f19e174a55f3c11451e374600dd0d4789
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-18 14:33:04 +0200

    alias Pokémon Catcher

---
+++ expansions/script/c26049095.lua
@@ -0,0 +1,15 @@
+--Pokémon Catcher (Emerging Powers 95/98)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	--switch
+	pm.EnableTrainerPlay(c,PM_CATEGORY_COIN,scard.swtg,scard.swop)
+end
+scard.pokemon_card=true
+function scard.swtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(pm.ActivePokemonFilter,tp,0,PM_LOCATION_ACTIVE,1,nil)
+		and Duel.IsExistingMatchingCard(pm.BenchPokemonFilter,tp,0,PM_LOCATION_IN_PLAY,1,nil) end
+end
+function scard.swop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.TossCoin(tp,1)==RESULT_HEADS then Duel.SwitchPokemon(e,tp,1-tp) end
+end
