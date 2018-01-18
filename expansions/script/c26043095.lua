b882f18389c31ddfeece0bb6a2c9d965ef9bffce
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-18 14:32:34 +0200

    alias Poké Ball

---
+++ expansions/script/c26043095.lua
@@ -0,0 +1,24 @@
+--Poké Ball (HeartGold & SoulSilver 95/123)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	--search
+	pm.EnableTrainerPlay(c,PM_CATEGORY_COIN,scard.thtg,scard.thop)
+end
+scard.pokemon_card=true
+function scard.thfilter(c)
+	return c:IsPokemon() and c:IsAbleToHand()
+end
+function scard.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+end
+function scard.thop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.TossCoin(tp,1)==RESULT_HEADS then
+		pm.ConfirmDeck(tp,tp)
+		Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_TOHAND)
+		local g=Duel.SelectMatchingCard(tp,scard.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+		if g:GetCount()==0 then return pm.ConfirmInvalid(tp,tp) end
+		Duel.SendtoHand(g,PLAYER_OWNER,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
