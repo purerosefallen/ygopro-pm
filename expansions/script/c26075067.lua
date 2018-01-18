f9f1625f34f0811c36b1e39c2757f3c4e19118df
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-18 14:31:33 +0200

    alias Switch

---
+++ expansions/script/c26075067.lua
@@ -0,0 +1,15 @@
+--Switch (Shining Legends 67/73)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	--switch
+	pm.EnableTrainerPlay(c,nil,scard.swtg,scard.swop)
+end
+scard.pokemon_card=true
+function scard.swtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(pm.ActivePokemonFilter,tp,PM_LOCATION_ACTIVE,0,1,nil)
+		and Duel.IsExistingMatchingCard(pm.BenchPokemonFilter,tp,PM_LOCATION_IN_PLAY,0,1,nil) end
+end
+function scard.swop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SwitchPokemon(e,tp,tp)
+end
