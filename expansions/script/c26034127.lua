84ec00b79d0f24d32b3ac289151fc3193e895f7e
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-18 14:29:52 +0200

    alias Potion

---
+++ expansions/script/c26034127.lua
@@ -0,0 +1,21 @@
+--Potion (Secret Wonders 127/132)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	--heal
+	pm.EnableTrainerPlay(c,nil,scard.healtg,scard.healop)
+end
+scard.pokemon_card=true
+function scard.healfilter(c)
+	return c:IsFaceup() and c:GetCounter(PM_DAMAGE_COUNTER)>0
+end
+function scard.healtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(scard.healfilter,tp,PM_LOCATION_IN_PLAY,0,1,nil) end
+end
+function scard.healop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_HEAL)
+	local g=Duel.SelectMatchingCard(tp,scard.healfilter,tp,PM_LOCATION_IN_PLAY,0,1,1,nil)
+	if g:GetCount()==0 then return end
+	Duel.HintSelection(g)
+	Duel.HealDamage(e,30,g:GetFirst())
+end
