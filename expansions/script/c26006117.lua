8aef70487f74f73ddd807d8a58c6cbed0e8810c3
Hello23-Ygopro <loleh32@hotmail.com>
2018-01-21 01:07:00 +0200

    new card special
    Added a card that allows a player to re-flip a coin for their Pokémon's attacks:
    - Sabrina's ESP (Gym Heroes 117/132)
    https://github.com/Hello23-Ygopro/ygopro-pm/issues/4

---
+++ expansions/script/c26006117.lua
@@ -0,0 +1,31 @@
+--Sabrina's ESP (Gym Heroes 117/132)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnableTrainerAttach(c,0,scard.cfilter,PM_LOCATION_IN_PLAY,0,aux.Stringid(sid,1),true)
+	--re-flip coin
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(sid,3))
+	e1:SetType(PM_EFFECT_TYPE_ATTACHED_TRAINER+EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(PM_EVENT_TOSS_COIN_NEGATE)
+	e1:SetRange(PM_LOCATION_IN_PLAY)
+	e1:SetCondition(scard.coincon)
+	e1:SetOperation(scard.coinop)
+	c:RegisterEffect(e1)
+end
+function scard.cfilter(c)
+	return c:IsFaceup() and c:IsPokemon() and c:IsSetCard(PM_SETNAME_SABRINA)
+end
+function scard.coincon(e,tp,eg,ep,ev,re,r,rp)
+	local code=e:GetHandler():GetOriginalCode()
+	return ep==tp and Duel.GetFlagEffect(tp,code)==0 and re:IsHasProperty(PM_EFFECT_FLAG_POKEMON_ATTACK)
+end
+function scard.coinop(e,tp,eg,ep,ev,re,r,rp)
+	local code=e:GetHandler():GetOriginalCode()
+	if Duel.GetFlagEffect(tp,code)~=0 then return end
+	if Duel.SelectYesNo(tp,aux.Stringid(code,2)) then
+		Duel.Hint(HINT_CARD,0,code)
+		Duel.RegisterFlagEffect(tp,code,RESET_PHASE+PHASE_END,0,1)
+		Duel.TossCoin(tp,ev)
+	end
+end
