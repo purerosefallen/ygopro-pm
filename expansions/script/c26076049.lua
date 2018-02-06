3d136b99f9e2977343c8b2a4b5046a7fe31c456e
Hello23-Ygopro <loleh32@hotmail.com>
2018-02-07 00:10:35 +0200

    add Pokémon-GX
    Added a Pokémon-GX and an Ultra Beast card:
    - Snorlax-GX (Black Star Promo SM05)
    - Nihilego-GX (Crimson Invasion 49/111)

---
+++ expansions/script/c26076049.lua
@@ -0,0 +1,45 @@
+--Nihilego-GX (Crimson Invasion 49/111)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnablePokemonAttribute(c)
+	--confused & poisoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(sid,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(PM_EVENT_PLAY_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(scard.spcon)
+	e1:SetTarget(pm.hinttg)
+	e1:SetOperation(scard.spop)
+	c:RegisterEffect(e1)
+	--cannot retreat
+	pm.EnablePokemonAttack(c,1,nil,scard.attack_cost,pm.hinttg,scard.effop)
+	--gx attack (to prize)
+	pm.EnablePokemonAttack(c,2,nil,scard.attack_cost,pm.hinttg,scard.tpop,nil,PM_EFFECT_FLAG_GX_ATTACK)
+end
+scard.pokemon_card=true
+scard.weakness_x2=PM_ENERGY_PSYCHIC
+scard.attack_cost=pm.econ1(CARD_PSYCHIC_ENERGY,3)
+function scard.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+end
+function scard.spop(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttackingPokemon()
+	local d=Duel.GetDefendingPokemon()
+	pm.EnableConfused(a)
+	pm.EnablePoisoned(a)
+	pm.EnableConfused(d)
+	pm.EnablePoisoned(d)
+end
+function scard.effop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.AttackDamage(120)
+	local tc=Duel.GetDefendingPokemon()
+	pm.EnableCannotRetreat(tc,aux.Stringid(sid,3),RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+end
+function scard.tpop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.PokemonAttack(e:GetHandler(),Duel.GetDefendingPokemon())
+	local g=Duel.GetDecktopGroup(1-tp,2)
+	if g:GetCount()==0 then return end
+	Duel.SendtoPrize(e,g,1-tp,REASON_EFFECT)
+end
---
+++ expansions/script/c2629005.lua
@@ -0,0 +1,29 @@
+--Snorlax-GX (Black Star Promo SM05)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnablePokemonAttribute(c)
+	--asleep
+	pm.EnablePokemonAttack(c,0,nil,scard.attack_cost1,pm.hinttg,scard.aslop1)
+	--damage
+	pm.EnablePokemonAttack(c,1,PM_CATEGORY_ASLEEP_ATTACK,scard.attack_cost2,pm.hinttg,scard.damop)
+	--gx attack (asleep)
+	pm.EnablePokemonAttack(c,2,nil,scard.attack_cost2,pm.hinttg,scard.aslop2,nil,PM_EFFECT_FLAG_GX_ATTACK)
+end
+scard.pokemon_card=true
+scard.weakness_x2=PM_ENERGY_FIGHTING
+scard.attack_cost1=pm.econ1(CARD_COLORLESS_ENERGY,3)
+scard.attack_cost2=pm.econ1(CARD_COLORLESS_ENERGY,5)
+function scard.aslop1(e,tp,eg,ep,ev,re,r,rp)
+	Duel.AttackDamage(80)
+	pm.EnableAsleep(e:GetHandler())
+end
+function scard.damop(e,tp,eg,ep,ev,re,r,rp)
+	local dam=180
+	if not e:GetHandler():IsAsleep() then dam=0 end
+	Duel.AttackDamage(dam)
+end
+function scard.aslop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.AttackDamage(210)
+	pm.EnableAsleep(e:GetHandler())
+end
