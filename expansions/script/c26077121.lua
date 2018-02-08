6039b3755bf7b83c66ee9e655293c3a517b5e0ab
Hello23-Ygopro <loleh32@hotmail.com>
2018-02-09 00:29:45 +0200

    new cards Electric Memory and Fire Memory
    - Electric Memory (Ultra Prism 121/156)
    - Fire Memory (Ultra Prism 123/156)

---
+++ expansions/script/c26077121.lua
@@ -0,0 +1,17 @@
+--Electric Memory (Ultra Prism 121/156)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnablePokemonToolAttribute(c,0)
+	--add type
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(PM_EFFECT_TYPE_POKEMON_TOOL)
+	e1:SetCode(PM_EFFECT_ADD_POKEMON_TYPE)
+	e1:SetCondition(scard.typecon)
+	e1:SetValue(PM_ENERGY_LIGHTNING)
+	c:RegisterEffect(e1)
+end
+scard.pokemon_card=true
+function scard.typecon(e)
+	return e:GetHandler():IsCode(CARD_SILVALLY_GX)
+end
---
+++ expansions/script/c26077123.lua
@@ -0,0 +1,17 @@
+--Fire Memory (Ultra Prism 123/156)
+local pm=require "expansions.utility_ptcg"
+local scard,sid=pm.GetID()
+function scard.initial_effect(c)
+	pm.EnablePokemonToolAttribute(c,0)
+	--add type
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(PM_EFFECT_TYPE_POKEMON_TOOL)
+	e1:SetCode(PM_EFFECT_ADD_POKEMON_TYPE)
+	e1:SetCondition(scard.typecon)
+	e1:SetValue(PM_ENERGY_FIRE)
+	c:RegisterEffect(e1)
+end
+scard.pokemon_card=true
+function scard.typecon(e)
+	return e:GetHandler():IsCode(CARD_SILVALLY_GX)
+end
