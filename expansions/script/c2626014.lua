1bb29e50e1c6503d5b2104adba5e4ce311a81914
Hello23-Ygopro <loleh32@hotmail.com>
2018-02-09 00:28:13 +0200

    fix Team Magma's Aggron
    Not having a Fighting-type Pokémon In Play shouldn't prevent *Team Magma's Aggron's* attack from being usable.

--- expansions/script/c2626014.lua
+++ expansions/script/c2626014.lua
@@ -6,7 +6,7 @@ function scard.initial_effect(c)
 	--evolve
 	pm.EnableEvolutionAttribute(c)
 	--damage
-	pm.EnablePokemonAttack(c,0,nil,scard.attack_cost1,scard.damtg,scard.damop1)
+	pm.EnablePokemonAttack(c,0,nil,scard.attack_cost1,pm.hinttg,scard.damop1)
 	--damage
 	pm.EnablePokemonAttack(c,1,nil,scard.attack_cost2,pm.hinttg,scard.damop2)
 end
@@ -16,27 +16,21 @@ scard.devolve_list={[1]=CARD_TEAM_MAGMAS_LAIRON,[2]=CARD_TEAM_MAGMAS_ARON}
 scard.weakness_x2=PM_ENERGY_GRASS
 scard.attack_cost1=pm.econ2(CARD_FIGHTING_ENERGY,1,CARD_COLORLESS_ENERGY,1)
 scard.attack_cost2=pm.econ2(CARD_FIGHTING_ENERGY,3,CARD_COLORLESS_ENERGY,1)
-function scard.damfilter1(c)
-	return c:IsFaceup() and c:IsPokemon() and c:GetAttachmentGroup():IsExists(Card.IsEnergy,1,nil,CARD_FIGHTING_ENERGY)
-end
-function scard.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(scard.damfilter1,tp,PM_LOCATION_IN_PLAY,0,1,nil) end
-	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
-end
 function scard.damop1(e,tp,eg,ep,ev,re,r,rp)
 	local ag=Duel.GetAttachmentGroup(tp,1,0)
 	local max=ag:FilterCount(Card.IsEnergy,nil,CARD_FIGHTING_ENERGY)
+	if max==0 then return Duel.PokemonAttack(e:GetHandler(),Duel.GetDefendingPokemon()) end
 	Duel.Hint(HINT_SELECTMSG,tp,PM_HINTMSG_DISCARDENERGY)
 	local sg=ag:FilterSelect(tp,Card.IsEnergy,1,max,nil,CARD_FIGHTING_ENERGY)
-	local ct=Duel.SendtoDPile(sg,REASON_EFFECT+REASON_DISCARD)
-	Duel.AttackDamage(40*ct)
+	local dam=Duel.SendtoDPile(sg,REASON_EFFECT+REASON_DISCARD)
+	Duel.AttackDamage(40*dam)
 end
-function scard.damfilter2(c)
+function scard.damfilter(c)
 	return pm.BenchPokemonFilter(c) and c:GetCounter(PM_DAMAGE_COUNTER)>0
 end
 function scard.damop2(e,tp,eg,ep,ev,re,r,rp)
 	Duel.AttackDamage(120)
-	local g=Duel.GetMatchingGroup(scard.damfilter2,tp,0,PM_LOCATION_IN_PLAY,nil)
+	local g=Duel.GetMatchingGroup(scard.damfilter,tp,0,PM_LOCATION_IN_PLAY,nil)
 	if g:GetCount()==0 then return end
 	local tc=g:GetFirst()
 	for tc in aux.Next(g) do
